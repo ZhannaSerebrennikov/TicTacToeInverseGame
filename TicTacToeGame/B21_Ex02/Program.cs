4ed9ebdd56c70e552f597274b05ea2864432f34b
ZhannaSerebrennikov <jserebrennikov@gmail.com>
2021-04-24 20:10:32 +0300

    project added

---
+++ TicTacToeGame/B21_Ex02/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace B21_Ex02
+{
+    class Program
+    {
+        static int m_width = 0;
+        static int m_height = 0;
+        static int m_decision = 0;
+        public static void Main()
+        {
+            CreateGameBoard();
+            char[,] gameBoard = new char[m_width, m_height];
+            for(int i = 0; i<gameBoard.GetLength(0); i++)
+            {
+                for (int j = 0; j < gameBoard.GetLength(1); j++)
+                {
+                    gameBoard[i, j] = ' ';
+                }
+            }
+            UI.PrintGameBoard(ref gameBoard);
+        }
+        public static void CreateGameBoard()
+        {
+            UI.GetWidthAndHeightFromUser(ref m_width, ref m_height);
+            while (m_width != m_height || m_width < 3 || m_width > 9)
+            {
+                UI.PrintErrorMessage();
+                UI.GetWidthAndHeightFromUser(ref m_width, ref m_height);
+            }
+
+            //=======not correct place ===========
+            UI.IfGameAgainstCompDecision(ref m_decision);
+            while (m_decision == 99)//???????????
+            {
+                UI.PrintErrorMessage();
+                UI.IfGameAgainstCompDecision(ref m_decision);
+            }
+            //===========================================
+        }
+    }
+
+
+}
---
+++ TicTacToeGame/B21_Ex02/UI.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Text;
+
+namespace B21_Ex02
+{
+    public class UI
+    {
+        public static void GetWidthAndHeightFromUser(ref int io_width, ref int io_height)
+        {
+           // System.Console.WriteLine("Hello! Welcome to Tic Tac Toe inverse game. Please enter width and hight of your matrix! The input should be digits, matrix shold have shape of squre and it shold be not less than 3x3 and not bigger than 9x9.");
+            string msg = string.Format(@"Hello!
+Welcome to Tic Tac Toe inverse game.
+Please enter width and hight of your matrix following the next rools:
+1.The input numers should be digits.
+2.Matrix shold have shape of squre.
+3.Width and height of matrix shold be not less than 3x3.
+4.Width and height of matrix shold be not bigger than 9x9.");
+            System.Console.WriteLine(msg);
+            System.Console.WriteLine("Please enter width of matrix: ");
+            string width = System.Console.ReadLine();
+            if(isAllDigits(width))
+            {
+                io_width = Int32.Parse(width);
+            }
+            System.Console.WriteLine("Please enter height of matrix: ");
+            string height = System.Console.ReadLine();
+            if (isAllDigits(height))
+            {
+                io_height = Int32.Parse(height);
+            }
+            
+        }
+
+        public static void PrintErrorMessage()
+        {
+            System.Console.WriteLine("User input was incorrect. Please try againe.");
+        }
+
+        public static void IfGameAgainstCompDecision(ref int io_decision)
+        {
+            System.Console.WriteLine("Press 0 if the game of two players or press 1 if the game against the computer?");
+            string decision = System.Console.ReadLine();
+            io_decision = Int32.Parse(decision);
+
+            if (!isAllDigits(decision) || io_decision <0 || io_decision>1)
+            {
+                io_decision = 99;
+            }
+        }
+
+        public static void PrintGameBoard(ref char[,] gameBoard)
+        {
+            StringBuilder newString = new StringBuilder();
+            int rows = gameBoard.GetLength(0);
+            int cols = gameBoard.GetLength(1);
+
+            for (int i = 0; i< cols; i++)
+            {
+                //newString.Append("   {0}", i + 1);
+                System.Console.Write("   {0}", i+1);
+            }
+            System.Console.WriteLine();
+            for (int i = 0; i< rows; i++)
+            {
+                System.Console.Write("{0}|", i + 1);
+                for (int j = 0; j < cols; j++)
+                {
+                    System.Console.Write(" {0} |", gameBoard[i,j]);
+                }
+                System.Console.WriteLine();
+                System.Console.Write(" ");
+                for (int j = 0; j < cols; j++)
+                {
+                    System.Console.Write("====");
+                }
+                System.Console.Write("=");
+                System.Console.WriteLine();
+            }
+
+        }
+
+        private static bool isAllDigits(string i_myString)
+        {
+            bool v_isAllDigits = true;
+            foreach (char c in i_myString)
+            {
+                if (!char.IsDigit(c))
+                {
+                    v_isAllDigits = false;
+                }
+            }
+
+            return v_isAllDigits;
+        }
+
+    }
+}
